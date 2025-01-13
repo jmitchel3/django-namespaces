aca0dcb142053c7bb9412f09182bd67721e951f9
Justin Mitchel <jmitchel3@gmail.com>
2025-01-13 12:42:40 -0700

    Improved versioning and dev env

--- src/django_namespaces/__init__.py
+++ src/django_namespaces/__init__.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+__version__ = "0.0.19"
+
 from django_namespaces.namespaces import activate, clear
 
-__all__ = ['activate', 'clear']
+__all__ = ["activate", "clear"]
